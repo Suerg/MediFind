7f397fbb3c891e2f23359b5749f850b6e0172c1b
David Piper <dbpiper@gmail.com>
2017-03-08 16:59:09 -0600

    Starting from scratch on MediFind. Create Welcome screen.

---
+++ MediFind/app/src/main/java/com/suerg/medifind/MainActivity.java
@@ -0,0 +1,24 @@
+package com.suerg.medifind;
+
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        View decorView = getWindow().getDecorView();
+        // Hide the status bar.
+        int uiOptions = View.SYSTEM_UI_FLAG_FULLSCREEN;
+        decorView.setSystemUiVisibility(uiOptions);
+        // Remember that you should never show the action bar if the
+        // status bar is hidden, so hide that too if necessary.
+        ActionBar actionBar = getSupportActionBar();
+        actionBar.hide();
+    }
+}
