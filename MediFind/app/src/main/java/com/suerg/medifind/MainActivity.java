b4c9928b47ce4cfc76fcd1ebd2bc8ca6e070c204
David Piper <dbpiper@gmail.com>
2017-03-09 07:18:52 -0600

    Change accent color to white to make the tabs more noticeable. Don't hide status bar as this is a bad UX practice.

--- MediFind/app/src/main/java/com/suerg/medifind/MainActivity.java
+++ MediFind/app/src/main/java/com/suerg/medifind/MainActivity.java
@@ -13,10 +13,10 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        View decorView = getWindow().getDecorView();
-        // Hide the status bar.
-        int uiOptions = View.SYSTEM_UI_FLAG_FULLSCREEN;
-        decorView.setSystemUiVisibility(uiOptions);
+//        View decorView = getWindow().getDecorView();
+//        // Hide the status bar.
+//        int uiOptions = View.SYSTEM_UI_FLAG_FULLSCREEN;
+//        decorView.setSystemUiVisibility(uiOptions);
         // Remember that you should never show the action bar if the
         // status bar is hidden, so hide that too if necessary.
         ActionBar actionBar = getSupportActionBar();
